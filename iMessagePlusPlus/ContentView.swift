895a0841ddd359bdae1c8b79b465815e8e2f2a73
Morgan <89803467+marinome@users.noreply.github.com>
2023-04-03 00:17:38 -0500

    with link

--- iMessagePlusPlus/ContentView.swift
+++ iMessagePlusPlus/ContentView.swift
@@ -15,6 +15,7 @@ struct ContentView: View {
         CodeEditor(source: $source, language: .cpp, theme: .ocean,  flags: [ .selectable, .editable, .smartIndent], autoPairs: [ "{": "}", "'": "'" ])
             .toolbar {
                 ToolbarItemGroup(placement: .keyboard) {
+                    //https://developer.apple.com/documentation/swiftui/view/toolbar(content:)-5w0tj
                 }
             }
     }
@@ -25,3 +26,19 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+/* static private let initialSource = "let a = 42\n"
+ 
+ @State private var source = Self.initialSource
+ @State private var selection = Self.initialSource.endIndex..<Self.initialSource.endIndex
+
+ var body: some View {
+     CodeEditor(source: $source,
+                selection: $selection,
+                language: .swift,
+                theme: .ocean,
+                autoscroll: false)
+     Button("Select All") {
+         selection = source.startIndex..<source.endIndex
+     }
+ }*/
