70bc53bb7d43148d601631e778a0b0faee66fd88
dshakamuri <43610752+dshakamuri@users.noreply.github.com>
2023-04-23 20:40:40 -0500

    Update ViewController.swift

--- iMessagePlusPlus/ViewController.swift
+++ iMessagePlusPlus/ViewController.swift
@@ -8,14 +8,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
+    // Override the viewDidLoad() method from the superclass to perform additional setup after the view has been loaded
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Define the attributes for the title and large title text of the navigation bar
         let attributes = [NSAttributedString.Key.font: UIFont(name: "Courier New-Bold", size: 30)!]
         let lattributes = [NSAttributedString.Key.font: UIFont(name: "Courier New-Bold", size: 24)!]
+        // Set the appearance of the navigation bar to match the defined attributes
         UINavigationBar.appearance().titleTextAttributes = attributes
         UINavigationBar.appearance().largeTitleTextAttributes = lattributes
+         // Set the background color of the navigation bar to a predefined color
         UINavigationBar.appearance().backgroundColor = UIColor(named: "LightGray")
     }
 }
